4f7cb34860c4fdb9b0d3bec5a58440e174a66711
sachin supunthaka <sachinsupunthaka96@gmail.com>
2023-12-08 09:37:21 +0530

    chor: data fetch on route and share to views

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,35 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcome', [
+        'heading'=> 'Latest Listings',
+        'listings' => [
+            [
+                'id'=>1,
+                'title'=>'Listing one',
+                'description'=>'This is desciption 1'
+            ],
+            [
+                'id'=>2,
+                'title'=>'Listing two',
+                'description'=>'This is desciption 2'
+            ],
+        ]
+    ]);
 });
+
+// Route::get('/hello', function(){
+//     return response('<h1>hello world</h1>')
+//         ->header('foo','bar');
+// });
+// // routes with wildcard ids
+// Route::get('/posts/{id}', function($id){
+//     return response('post ' . $id)
+//         ->header('foo','bar');
+// })->where('id', '[0-9]+');
+
+// // make sure o pass the $sign
+// // when using on functions
+// Route::get('/search', function(Request $request){
+//     dd($request-> name .' '. $request->city);
+// });
